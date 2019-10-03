23fab70da1f4863246ae87eee724a14f090e2128
Kevin <kevinalexanderduran8@gmail.com>
2019-10-03 15:14:18 -0700

    finished adding animation to nav button

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
 ScrollReveal().reveal('.reveal_me', { delay: 500 });
+
+
+//Navigation Overlay
+$('#toggle').click(function () {
+    $(this).toggleClass('active');
+    $('#overlay').toggleClass('open');
+  });
+  //End of Navigarion Overlay
