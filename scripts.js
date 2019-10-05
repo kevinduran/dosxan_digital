343436f91d3aa059550c7527e9f623bc5417f801
Kevin <kevinalexanderduran8@gmail.com>
2019-10-04 23:40:57 -0700

    added nav sticky

--- scripts.js
+++ scripts.js
@@ -1,7 +1,24 @@
+//Work section reveal
 ScrollReveal().reveal('.reveal_me', {
     delay: 500
 });
+//End of Work section reveal
 
+//Sticky Nav color change
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".nav");
+      var $navItem = $('.navigation_item');
+      var $navItemTwo = $('.navigation_item_two');
+
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      $navItem.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      $navItemTwo.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+
+    });
+  });
+
+//End of Sticky Nav color change
 
 //Navigation Overlay
 $('#toggle').click(function () {
