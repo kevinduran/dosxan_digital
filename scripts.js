271e53d5391780bca915070045dd070ced0c4101
Kevin <kevinalexanderduran8@gmail.com>
2019-10-10 19:25:26 -0700

    adding the two sticky items now

--- scripts.js
+++ scripts.js
@@ -10,11 +10,13 @@ $(function () {
     var $nav = $(".nav");
     var $navItem = $('.navigation_item');
     var $navItemTwo = $('.navigation_item_two');
+    var $navItemThree = $('.navigation_item_three');
+
 
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     $navItem.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     $navItemTwo.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-
+    $navItemThree.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
 });
 
