b7a3d226c41dd736a883a4f597942befcefc0927
Kevin <kevinalexanderduran8@gmail.com>
2020-04-02 15:30:02 -0700

    HEADER: added a javascript for word change.

--- scripts.js
+++ scripts.js
@@ -35,3 +35,18 @@ $('#toggle').click(function () {
 AOS.init({
   duration: 1200,
 })
+
+
+var text = ["create", "develop", "build"];
+var counter = 0;
+var elem = document.getElementById("changeText");
+var inst = setInterval(change, 1000);
+
+function change() {
+  elem.innerHTML = text[counter];
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+    // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+  }
+}
