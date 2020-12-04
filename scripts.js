df0a9955e4cca717a0b3a77d65cecc546267554b
Kevin <kevinalexanderduran8@gmail.com>
2020-12-04 07:00:34 -0800

    test

--- scripts.js
+++ scripts.js
@@ -36,9 +36,10 @@ var text = ["create", "develop", "build", "shape"];
 var counter = 0;
 var elem = document.getElementById("changeText");
 var header_word = document.querySelector('.intro--header-word-fill')
-var initTwo = setInterval(colorDown, 100);
-var init = setInterval(change, 200);
-var initThree = setInterval(colorUp, 150);
+
+setInterval(colorDown, 1000);
+setInterval(change, 2000);
+setInterval(colorUp, 2000);
 function change() {
   elem.innerHTML = text[counter];
   counter++;
